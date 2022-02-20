edb863ffcd1afd1570be6604e286be71f412091c
George Delchev <george.delchev044@outlook.com>
2022-02-20 12:59:36 +0200

    before adding cardData to card creation

--- js/main.js
+++ js/main.js
@@ -1,13 +1,147 @@
-window.addEventListener('load', () => {
-    addCardsToArea();
-});
+let numberOfTries = 0;
+let numberOfCards = 8;
+
+class CardPair {
+    constructor() {
+        this.cardOne = new Card();
+        this.cardTwo = new Card();
+        this.cardHolderOne = this.createCardPair(this.cardOne);
+        this.cardHolderTwo = this.createCardPair(this.cardTwo);
+        //this.cardHolderTwo = this.cardHolderOne.cloneNode(true);
+        this.matched = false;
+    }
+    createCardPair = function (card) {
+        console.log('createCardPair:'+card)
+        const newCardHolder = document.createElement('div');
+        newCardHolder.classList.add('cardHolder');
+
+        newCardHolder.appendChild(card.frontSide);
+        newCardHolder.appendChild(card.backSide);
+        return newCardHolder;
+    }
+}
+class Card {
+    constructor() {
+        this.frontSide = this.createFrontSide();
+        this.backSide = this.createBackSide();
+        this.setCardFrontSide();
+    }
+    createFrontSide = function () {
+        const newCardFront = document.createElement('div');
+        newCardFront.classList.add('card');
+        newCardFront.classList.add('card-rotate-opposite');
+        return newCardFront;
+    }
+    createBackSide = function () {
+        const newCardBack = document.createElement('div');
+        newCardBack.classList.add('card');
+        newCardBack.classList.add('card-back');
+        newCardBack.addEventListener('click', checkForMatch);
+        return newCardBack;
+    }
+    setCardFrontSide = async function () {
+        switch (cardType) {
+            case 'color':
+                this.frontSide.style.background = getRandomColor();
+                break;
+            case 'cards':
+                {
+                    const url = `https://www.deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`;
+                    fetch(url)
+                        .then(res => res.json())
+                        .then(data => {
+                            this.frontSide.style.backgroundImage = `url(${data.cards[0].image})`;
+                        })
+                        .catch(err => {
+                            alert(`error: ${err}`);
+                        });
+                }
+                break;
+            default:
+                alert("WRONG CARD TYPE");
+                break;
+        }
+    }
+}
 const cardsArea = document.getElementById('cardsArea');
+function makeACardTest() {
+    let cardPair = new CardPair();
+    console.log(cardPair);
+    cardsArea.appendChild(cardPair.cardHolderOne);
+    cardsArea.appendChild(cardPair.cardHolderTwo);
+}
+//set type of card
+let cardType = 'cards';
+let deckId;
+const cardsTypeButtons = document.querySelectorAll('.cardType');
+cardsTypeButtons.forEach(e => e.addEventListener('change', setCardType));
+function setCardType(e) {
+    cardType = e.target.value;
+    addCardsToArea();
+    if (cardType === 'cards') {
+        shuffleCardDeckAPI();
+    }
+}
+window.addEventListener('load', () => {
+    // addCardsToArea();
+    getCardDeckAPI();
+    makeACardTest();
+});
+function getCardDeckAPI() {
+    deckId = localStorage.getItem('deckId');
+
+    if (!deckId) {
+        const url = `https://www.deckofcardsapi.com/api/deck/new/shuffle`;
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                deckId = data.deck_id;
+                localStorage.setItem('deckId', deckId);
+            })
+            .catch(err => {
+                alert(`error: ${err}`);
+            });
+    }
+}
+function shuffleCardDeckAPI() {
+    if (deckId) {
+        const url = `https://www.deckofcardsapi.com/api/deck/${deckId}/shuffle/`;
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                console.log(`Deck ${data.deck_id} shuffled: ${data.shuffled}`)
+            })
+            .catch(err => {
+                alert(`error: ${err}`);
+            });
+        getCardFromDeckAPI();
+    }
+    else {
+        getCardDeckAPI();
+    }
+}
+async function getCardFromDeckAPI() {
+    if (deckId) {
+        const url = `https://www.deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`;
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                return data;
+            })
+            .catch(err => {
+                alert(`error: ${err}`);
+            });
+    }
+    else {
+        getCardDeckAPI();
+        getCardFromDeckAPI();
+    }
+}
+
 const numberOfTriesSpan = document.getElementById('numberOfTries');
 
-let numberOfTries = 0;
-let typeOfCard = 'color';
 let delayTime = 500;
-let numberOfCards = 8;
 
 const cardsButtons = document.querySelectorAll('.getCardButton');
 cardsButtons.forEach(element => {
@@ -16,14 +150,14 @@ cardsButtons.forEach(element => {
         addCardsToArea()
     });
 });
-const cardsTypeButtons = document.querySelectorAll('.cardType');
-console.log(cardsTypeButtons)
-cardsTypeButtons.forEach(element => {
-    element.addEventListener('click', addCardsToArea);
-});
+// const cardsTypeButtons = document.querySelectorAll('.cardType');
+// //console.log(cardsTypeButtons)
+// cardsTypeButtons.forEach(element => {
+//     element.addEventListener('click', addCardsToArea);
+// });
 
 function addCardsToArea() {
-    console.log('addCardsToArea')
+    //console.log('addCardsToArea')
     numberOfTries = 0;
     numberOfTriesSpan.innerText = 0;
     cardsArea.innerHTML = '';
@@ -46,8 +180,8 @@ function addCardsToArea() {
     let promise = Promise.resolve();
     cardArray.forEach(function (element) {
         promise = promise.then(function () {
-        cardsArea.appendChild(element);
-        return new Promise(function (resolve) {
+            cardsArea.appendChild(element);
+            return new Promise(function (resolve) {
                 setTimeout(resolve, 50);
             });
         });
@@ -75,7 +209,7 @@ function createCard(cardAttribute, pair) {
     newCardBack.classList.add('card-back');
     newCardBack.classList.add('incorrect');
     newCardBack.setAttribute("card", cardAttribute);
-    newCardBack.setAttribute("pair", cardAttribute+pair);
+    newCardBack.setAttribute("pair", cardAttribute + pair);
     newCardBack.setAttribute("tabindex", 0);
     newCardBack.addEventListener('click', checkForMatch);
     newCardHolder.appendChild(newCardBack);
@@ -86,7 +220,7 @@ function createCard(cardAttribute, pair) {
     newCardFront.classList.add('card-rotate-opposite');
     newCardFront.style.background = setCardFrontSide(cardAttribute);
     newCardFront.setAttribute("card", cardAttribute);
-    newCardFront.setAttribute("pair", cardAttribute+pair);
+    newCardFront.setAttribute("pair", cardAttribute + pair);
     newCardFront.setAttribute("tabindex", 0);
     newCardHolder.appendChild(newCardFront);
 
@@ -179,6 +313,7 @@ function getRandomCard() {
 
 }
 function setCardFrontSide(cardAttribute) {
+
     var cardTypesElements = document.querySelector('.cardType:checked');
     typeOfCard = cardTypesElements.value;
 
@@ -187,6 +322,7 @@ function setCardFrontSide(cardAttribute) {
             return cardAttribute;
         case 'cards':
             let cardNumber = parseInt(cardAttribute);
+            //console.log(cardNumber);
             return `rgb(${cardNumber}, 0, 0)`;
         default:
             alert("WRONG CART TYPE");
